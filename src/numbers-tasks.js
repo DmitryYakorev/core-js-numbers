7e58415145926038a8fba9777a5b380825873c6d
Dmitry Yakorev <urakend@mail.ru>
2023-11-16 21:26:22 +0300

    feat:new1

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -294,8 +294,12 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  const res2 = num
+    .toString()
+    .split('')
+    .reduce((acc, el) => acc + Number(el), 0);
+  return res2;
 }
 
 /**
